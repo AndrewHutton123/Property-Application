f1d6da5efce04850704d5e07ad101f38810a0492
AndrewHutton123 <andrewhu11on123@gmail.com>
2024-08-07 20:27:42 +0100

    Added property cards and set up mongoDB

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
+  console.log(process.env.MONGODB_URI);
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
